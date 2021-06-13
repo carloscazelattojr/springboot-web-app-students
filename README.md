2d049c9465ab3e5aece135441fb12897d843b1f8
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-06-13 10:13:49 -0300

    Project created

---
+++ README.md
@@ -0,0 +1,17 @@
+# Students Management System using Spring Boot and Thymeleaf
+
+
+# Project:
+
+Spring Initializr: https://start.spring.io
+
+## Dependencies
+- Spring Boot DevTools
+- Spring Web
+- MySql Driver 
+- Spring Data JPA
+- Thymeleaf
+
+
+
+Créditos: JavaGuides
---
+++ src/main/java/br/com/carlosjunior/studentsms/StudentsMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.studentsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsMsApplication.class, args);
+	}
+
+}
