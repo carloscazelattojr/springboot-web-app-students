5d7456ffc492e0ee12e6d8f23e86599f2f4cda8e
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-06-13 10:41:36 -0300

    Entity and Repository of Student created

---
+++ src/main/java/br/com/carlosjunior/studentsms/entity/Student.java
@@ -0,0 +1,71 @@
+package br.com.carlosjunior.studentsms.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastname;
+
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	public Student() {
+
+	}
+
+	public Student(String firstName, String lastname, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastname = lastname;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ src/main/java/br/com/carlosjunior/studentsms/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package br.com.carlosjunior.studentsms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.carlosjunior.studentsms.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
